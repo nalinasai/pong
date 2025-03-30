28adab0f79bb35885f4315cae5ce0d488b1cd149
nalinasai <nalinanalina995@gmail.com>
2025-03-30 19:01:59 +0530

    other functions added

--- pong.py
+++ pong.py
@@ -1,9 +1,14 @@
 import turtle
+#import winsound
 window = turtle.Screen()
 window.bgcolor("black")
 window.title("PONG GAME!!")
 window.setup(width=800,height=600)
 window.tracer(0) 
+
+#score
+score_a = 0
+score_b = 0
 
 
 #paddle a
@@ -34,6 +39,17 @@ ball.speed(0)
 ball.goto(0, 0)
 ball.dx = 0.2
 ball.dy = 0.2
+
+#pen
+pen = turtle.Turtle()
+pen.penup()
+pen.color("white")
+pen.goto(0,260)
+
+pen.speed(0)
+pen.hideturtle()
+pen.write("Player A: 0   Player B: 0",align="center",font=("Arial",24,"bold"))
+
 
 #function
 def paddle_a_up():
@@ -73,16 +89,36 @@ while True:
     #boarder checking
     if(ball.ycor()>290):
         ball.sety(290)
-        ball.dy *= -1
+        ball.dy *=-1
+        #winsound.PlaySound("ball-bounce-94853",winsound.SND_ASYNC)
 
     if(ball.ycor()<-290):
         ball.sety(-290)
         ball.dy *=-1
+        #winsound.PlaySound("ball-bounce-94853",winsound.SND_ASYNC)
 
     if(ball.xcor()>390):
         ball.goto(0,0)
         ball.dx*=-1
+        score_a+=1
+        pen.clear()
+        pen.write("Player A: {}   Player B: {}".format(score_a,score_b),align="center",font=("Arial",24,"bold"))
+        #winsound.PlaySound("ball-bounce-94853",winsound.SND_ASYNC)
 
     if(ball.xcor()<-390):
         ball.goto(0,0)
         ball.dx*=-1
+        score_b+=1
+        pen.clear()
+        pen.write("Player A: {}   Player B: {}".format(score_a,score_b),align="center",font=("Arial",24,"bold"))
+        #winsound.PlaySound("ball-bounce-94853",winsound.SND_ASYNC)
+
+
+    if(ball.xcor()>340 and ball.xcor()<350) and (ball.ycor()<paddle_b.ycor()+40 and ball.ycor()> paddle_b.ycor() -40):
+        ball.setx(340)
+        ball.dx *= -1
+    
+    if(ball.xcor()<-340 and ball.xcor()>-350) and (ball.ycor()<paddle_a.ycor()+40 and ball.ycor()> paddle_a.ycor() -40):
+        ball.setx(-340)
+        ball.dx *= -1
+
